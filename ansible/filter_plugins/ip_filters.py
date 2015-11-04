bebb3659fe196600052fd863d779ea83a306f381
Pedro Marques <pedro.r.marques@gmail.com>
2015-11-04 01:12:26 +0000

    Common opencontrail playbook between k8s and OpenShift.

---
+++ ansible/filter_plugins/ip_filters.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Copyright 2015 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+Custom filter used by opencontrail play to convert a network mask into
+prefix length.
+'''
+
+import netaddr
+
+class FilterModule(object):
+	''' Custom ansible filter '''
+
+	@staticmethod
+	def netmask2prefixlen(data):
+		net = netaddr.IPNetwork('0.0.0.0/%s' % data)
+		return net.prefixlen
+
+	def filters(self):
+		return {
+			"netmask2prefixlen": self.netmask2prefixlen
+		}
