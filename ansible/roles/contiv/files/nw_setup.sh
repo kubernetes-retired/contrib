787ac8acdd17bdffaf52115fd0bd01ba2518833d
jojimt <joji@cisco.com>
2016-01-13 07:51:07 -0800

    Reorder netplugin startup and add a tag for ovs_start

--- ansible/roles/contiv/files/nw_setup.sh
+++ ansible/roles/contiv/files/nw_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 30
+sleep 60
 #create default nw and epg
 /usr/bin/contivctl network create -public no -encap vxlan -subnet 20.1.1.0/24 -gateway 20.1.1.254 default-net
 /usr/bin/contivctl group create default-net default-epg
