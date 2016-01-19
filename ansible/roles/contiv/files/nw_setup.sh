9086a2305418934647b7326b080f351ad1e68ad0
jojimt <joji@cisco.com>
2016-01-19 09:20:22 -0800

    Removed default epg creation.

--- ansible/roles/contiv/files/nw_setup.sh
+++ ansible/roles/contiv/files/nw_setup.sh
@@ -3,7 +3,6 @@
 sleep 60
 #create default nw and epg
 /usr/bin/contivctl network create -public no -encap vxlan -subnet 20.1.1.0/24 -gateway 20.1.1.254 default-net
-/usr/bin/contivctl group create default-net default-epg
 
 
 #create poc nw and epg
