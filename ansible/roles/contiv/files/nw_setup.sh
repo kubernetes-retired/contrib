78ac1ae8494309db4c70a6dbfb768af6464640f7
jojimt <joji@cisco.com>
2016-01-21 06:15:37 -0800

    Fix k8s issue #395, replace contivctl with netctl

--- ansible/roles/contiv/files/nw_setup.sh
+++ ansible/roles/contiv/files/nw_setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sleep 60
 #create default nw and epg
-/usr/bin/contivctl network create -public no -encap vxlan -subnet 20.1.1.0/24 -gateway 20.1.1.254 default-net
+netctl net create default-net --subnet=20.1.1.0/24 --gateway=20.1.1.254
 
 
 #create poc nw and epg
-/usr/bin/contivctl network create -public no -encap vxlan -subnet 21.1.1.0/24 -gateway 21.1.1.254 poc-net
-/usr/bin/contivctl group create poc-net poc-epg
+netctl net create poc-net --subnet=21.1.1.0/24 --gateway=21.1.1.254
+netctl group create poc-net poc-epg
