3b654962236848a07a9c5484d2e546c62a818dc0
Marie Shaw <mnshaw@google.com>
2016-06-22 16:52:48 -0700

    get logs for failed pods in node e2e tests

---
+++ kube-logging/logging.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run script with the url of the GCS bucket as the first argument
+# Pulls all files in the bucket and puts them into a folder and
+# runs the rdjunit file
+url=$1
+bucket=${url#h*[e][r][/]}
+echo $bucket
+bucket=${bucket%/*}
+filename=${bucket##**/}
+datetime=$(date +"%m_%d_%Y-%H_%M_%S")
+filename=$filename-$datetime
+mkdir $filename
+cd $filename
+gslink=gs://$bucket/*
+gsutil -m cp -r $gslink .
+cd ..
+go run rdjunit.go $filename
