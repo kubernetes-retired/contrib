33553cdd701759a45a8ed434f5cc1a19f4dbd776
Madhusudan.C.S <madhusudancs@google.com>
2016-07-08 19:26:08 -0700

    Add modify config support for auth info and context.

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -161,34 +161,102 @@ func modifyConfig(configAccess clientcmd.ConfigAccess, suppliedConfig *clientcmd
 		return err
 	}
 
+	for name, authInfo := range suppliedConfig.AuthInfos {
+		initial, ok := config.AuthInfos[name]
+		if !ok {
+			initial = clientcmdapi.NewAuthInfo()
+		}
+		modifiedAuthInfo := *initial
+
+		var setToken, setBasic bool
+
+		if len(authInfo.ClientCertificate) > 0 {
+			modifiedAuthInfo.ClientCertificate = authInfo.ClientCertificate
+		}
+		if len(authInfo.ClientCertificateData) > 0 {
+			modifiedAuthInfo.ClientCertificateData = authInfo.ClientCertificateData
+		}
+
+		if len(authInfo.ClientKey) > 0 {
+			modifiedAuthInfo.ClientKey = authInfo.ClientKey
+		}
+		if len(authInfo.ClientKeyData) > 0 {
+			modifiedAuthInfo.ClientKeyData = authInfo.ClientKeyData
+		}
+
+		if len(authInfo.Token) > 0 {
+			modifiedAuthInfo.Token = authInfo.Token
+			setToken = len(modifiedAuthInfo.Token) > 0
+		}
+
+		if len(authInfo.Username) > 0 {
+			modifiedAuthInfo.Username = authInfo.Username
+			setBasic = setBasic || len(modifiedAuthInfo.Username) > 0
+		}
+		if len(authInfo.Password) > 0 {
+			modifiedAuthInfo.Password = authInfo.Password
+			setBasic = setBasic || len(modifiedAuthInfo.Password) > 0
+		}
+
+		// If any auth info was set, make sure any other existing auth types are cleared
+		if setToken || setBasic {
+			if !setToken {
+				modifiedAuthInfo.Token = ""
+			}
+			if !setBasic {
+				modifiedAuthInfo.Username = ""
+				modifiedAuthInfo.Password = ""
+			}
+		}
+		config.AuthInfos[name] = &modifiedAuthInfo
+	}
+
 	for name, cluster := range suppliedConfig.Clusters {
 		initial, ok := config.Clusters[name]
 		if !ok {
 			initial = clientcmdapi.NewCluster()
 		}
-		modified := *initial
+		modifiedCluster := *initial
 
 		if len(cluster.Server) > 0 {
-			modified.Server = cluster.Server
+			modifiedCluster.Server = cluster.Server
 		}
 		if cluster.InsecureSkipTLSVerify {
-			modified.InsecureSkipTLSVerify = cluster.InsecureSkipTLSVerify
+			modifiedCluster.InsecureSkipTLSVerify = cluster.InsecureSkipTLSVerify
 			// Specifying insecure mode clears any certificate authority
-			if modified.InsecureSkipTLSVerify {
-				modified.CertificateAuthority = ""
-				modified.CertificateAuthorityData = nil
+			if modifiedCluster.InsecureSkipTLSVerify {
+				modifiedCluster.CertificateAuthority = ""
+				modifiedCluster.CertificateAuthorityData = nil
 			}
 		}
 		if len(cluster.CertificateAuthorityData) > 0 {
-			modified.CertificateAuthorityData = cluster.CertificateAuthorityData
-			modified.InsecureSkipTLSVerify = false
-			modified.CertificateAuthority = ""
-		} else if len(cluster.CertificateAuthority) > 0 {
-			modified.CertificateAuthority = cluster.CertificateAuthority
-			modified.InsecureSkipTLSVerify = false
-			modified.CertificateAuthorityData = nil
+			modifiedCluster.CertificateAuthorityData = cluster.CertificateAuthorityData
+			modifiedCluster.InsecureSkipTLSVerify = false
 		}
-		config.Clusters[name] = &modified
+		if len(cluster.CertificateAuthority) > 0 {
+			modifiedCluster.CertificateAuthority = cluster.CertificateAuthority
+			modifiedCluster.InsecureSkipTLSVerify = false
+		}
+		config.Clusters[name] = &modifiedCluster
+	}
+
+	for name, context := range suppliedConfig.Contexts {
+		initial, ok := config.Contexts[name]
+		if !ok {
+			initial = clientcmdapi.NewContext()
+		}
+		modifiedContext := *initial
+
+		if len(context.Cluster) > 0 {
+			modifiedContext.Cluster = context.Cluster
+		}
+		if len(context.AuthInfo) > 0 {
+			modifiedContext.AuthInfo = context.AuthInfo
+		}
+		if len(context.Namespace) > 0 {
+			modifiedContext.Namespace = context.Namespace
+		}
+		config.Contexts[name] = &modifiedContext
 	}
 
 	if err := clientcmd.ModifyConfig(configAccess, *config, true); err != nil {
