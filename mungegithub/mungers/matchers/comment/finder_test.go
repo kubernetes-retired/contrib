261779676cb6336ffe5d4f87770c17452e12a87e
Antoine Pelisse <apelisse@google.com>
2016-08-25 13:57:27 -0700

    Ping people when flaky issues don't get attention

--- mungegithub/mungers/matchers/comment/finder_test.go
+++ mungegithub/mungers/matchers/comment/finder_test.go
@@ -19,7 +19,9 @@ package comment
 import (
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/golang/glog"
 	"github.com/google/go-github/github"
 )
 
@@ -40,4 +42,42 @@ func TestFilterComments(t *testing.T) {
 	if !reflect.DeepEqual([]*github.IssueComment(fullList), comments) {
 		t.Error("True filter should have kept every element")
 	}
+}
+
+func makeCommentWithCreatedAt(year int, month time.Month, day int) *github.IssueComment {
+	date := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+	return &github.IssueComment{
+		CreatedAt: &date,
+	}
+}
+
+func TestLastCommentDefault(t *testing.T) {
+	if LastComment(nil, True{}, nil) != nil {
+		t.Error("Empty list should return nil default")
+	}
+	if !reflect.DeepEqual(LastComment(nil, True{}, &time.Time{}), &time.Time{}) {
+		t.Error("Empty list should return given default value")
+	}
+}
+
+func TestLastComment(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeCommentWithCreatedAt(2000, 1, 1),
+		makeCommentWithCreatedAt(2000, 1, 2),
+		makeCommentWithCreatedAt(2000, 1, 3),
+	}
+	if !reflect.DeepEqual(*LastComment(comments, True{}, nil), time.Date(2000, 1, 3, 0, 0, 0, 0, time.UTC)) {
+		t.Error("Should match the last comment")
+	}
+}
+
+func (s *SizeMunger) isStaleComment(obj *github.MungeObject, comment *githubapi.IssueComment) bool {
+	if !mergeBotComment(comment) {
+		return false
+	}
+	stale := sizeRE.MatchString(*comment.Body)
+	if stale {
+		glog.V(6).Infof("Found stale SizeMunger comment")
+	}
+	return stale
 }
