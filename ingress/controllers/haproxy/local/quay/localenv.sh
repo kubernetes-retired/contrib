dcbe8dba69ba794e42960b95fee53dbcbdbd0be7
Pablo Mercado <odacremolbap@gmail.com>
2016-09-29 23:52:59 +0200

    remove local tests

--- ingress/controllers/haproxy/local/quay/localenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker login -e="." -u="odacremolbap" -p="9TzM4O5PSpxcq9ev/8MhBJY9WNDE2VpQXbiclcD55IKvjyq/zMbvuKqws2S36n8p" quay.io
-
-export IMAGE_SITE_USER=odacremolbap
-export IMAGE_SITE_PASSWORD=B1ll.fr1s3ll
-
