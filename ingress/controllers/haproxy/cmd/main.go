e06f019d7d273d1fe50f69d2c1aabab0d580aa1a
Pablo Mercado <odacremolbap@gmail.com>
2016-10-05 19:35:10 +0200

    add boilerplate header for kubernetes

--- ingress/controllers/haproxy/cmd/main.go
+++ ingress/controllers/haproxy/cmd/main.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2015 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -8,14 +24,14 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/golang/glog"
+	log "github.com/golang/glog"
+	"github.com/spf13/pflag"
 	"k8s.io/contrib/ingress/controllers/haproxy/cmd/model"
 	"k8s.io/contrib/ingress/controllers/haproxy/cmd/server"
 	"k8s.io/contrib/ingress/controllers/haproxy/pkg/balancer"
 	"k8s.io/contrib/ingress/controllers/haproxy/pkg/balancer/haproxy"
 	"k8s.io/contrib/ingress/controllers/haproxy/pkg/controller"
-	"github.com/golang/glog"
-	log "github.com/golang/glog"
-	"github.com/spf13/pflag"
 	"k8s.io/kubernetes/pkg/api"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 	"k8s.io/kubernetes/pkg/kubectl/cmd/util"
