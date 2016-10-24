54038d5edf4c35abd28bc4a58000b603c3c6abd7
Garrett Rodrigues <grod@google.com>
2016-10-24 15:47:18 -0700

    fixed the hasApproval implementation
    Now we allow for approval for any approver in path
    Also allow recognize approve cancel comment

--- mungegithub/mungers/approval-handler.go
+++ mungegithub/mungers/approval-handler.go
@@ -17,7 +17,6 @@ limitations under the License.
 package mungers
 
 import (
-	"path"
 	"strings"
 
 	"k8s.io/contrib/mungegithub/features"
@@ -26,10 +25,9 @@ import (
 	"k8s.io/kubernetes/pkg/util/sets"
 
 	"github.com/golang/glog"
+	goGithub "github.com/google/go-github/github"
 	"github.com/spf13/cobra"
 )
-
-const maxDepth = 3
 
 // ApprovalHandler will try to add "approved" label once
 // all files of change has been approved by approvers.
@@ -64,15 +62,17 @@ func (*ApprovalHandler) AddFlags(cmd *cobra.Command, config *github.Config) {}
 
 // Munge is the workhorse the will actually make updates to the PR
 // The algorithm goes as:
-// - Initially, we set up approverSet
-//   - Go through all comments after latest commit. If any approver said "/approve", add him to approverSet.
-// - For each file, we see if any approver of this file is in approverSet.
+// - Initially, we build two sets, an approverSet and a cancelSet
+//   - Go through all comments after latest commit.
+//	- If anyone said "/approve", add them to approverSet.
+//	- If anyone said "/approve cancel", add them to cancelSet.
+// - Then, for each file, we see if any approver of this file is in approverSet or cancelSet
 //   - An approver of a file is defined as:
-//     - It's known that each dir has a list of approvers. (This might not hold true. For usability, current situation is enough.)
-//     - Approver of a dir is also the approver of child dirs.
-//   - We look at top N (default 3) level dir approvers. For example, for file "/a/b/c/d/e", we might search for approver from
-//     "/", "/a/", "/a/b/"
-// - Iff all files has been approved, the bot will add "approved" label.
+//     - Someone listed as an "approver" in an OWNERS file in the files directory OR
+//     - in one of the file's parent directories
+//   - If a valid approver is in cancelSet, we remove the approve label (if there is one) and return immediately
+// - Iff all files have been approved, the bot will add the "approved" label.
+// - This implies that if any file doesn't have "approved" from a valid approver, the approval label will not be applied
 func (h *ApprovalHandler) Munge(obj *github.MungeObject) {
 	if !obj.IsPR() {
 		return
@@ -90,8 +90,31 @@ func (h *ApprovalHandler) Munge(obj *github.MungeObject) {
 	}
 
 	approverSet := sets.String{}
+	if obj.Issue.User.Name != nil {
+		approverSet.Insert(*obj.Issue.User.Name)
+	}
 
-	// from oldest to latest
+	approverSet = approverSet.Union(createApproverSet(comments))
+	needsApproval := h.getApprovalNeededFiles(files, approverSet)
+
+	if needsApproval.Len() > 0 {
+		//need to selectively write a comment explaining why approved not added
+		glog.Infof("Canceling the approve label because these files %v have no valid approvers", needsApproval)
+		if obj.HasLabel(approvedLabel) {
+			obj.RemoveLabel(approvedLabel)
+		}
+	} else if !obj.HasLabel(approvedLabel) {
+		obj.AddLabel(approvedLabel)
+	}
+
+}
+
+// createApproverSet iterates through the list of comments on a PR
+// and identifies all of the people that have said /approve and adds
+// them to the approverSet.  The function uses the latest approve or cancel comment
+// to determine the Users intention
+func createApproverSet(comments []*goGithub.IssueComment) sets.String {
+	approverSet := sets.String{}
 	for i := len(comments) - 1; i >= 0; i-- {
 		c := comments[i]
 
@@ -103,42 +126,28 @@ func (h *ApprovalHandler) Munge(obj *github.MungeObject) {
 
 		if len(fields) == 1 && strings.ToLower(fields[0]) == "/approve" {
 			approverSet.Insert(*c.User.Login)
-			continue
-		}
-
-		if len(fields) == 2 && strings.ToLower(fields[0]) == "/approve" && strings.ToLower(fields[1]) == "cancel" {
-			approverSet.Delete(*c.User.Login)
-		}
-	}
-
-	for _, file := range files {
-		if !h.hasApproval(*file.Filename, approverSet, maxDepth) {
-			return
-		}
-	}
-	obj.AddLabel(approvedLabel)
-}
-
-func (h *ApprovalHandler) hasApproval(filename string, approverSet sets.String, depth int) bool {
-	paths := strings.Split(filename, "/")
-	p := ""
-	for i := 0; i < len(paths) && i < depth; i++ {
-		fileOwners := h.features.Repos.LeafAssignees(p)
-		if fileOwners.Len() == 0 {
-			glog.Warningf("Couldn't find an owner for path (%s)", p)
-			continue
-		}
-
-		if h.features.Aliases != nil && h.features.Aliases.IsEnabled {
-			fileOwners = h.features.Aliases.Expand(fileOwners)
-		}
-
-		for _, owner := range fileOwners.List() {
-			if approverSet.Has(owner) {
-				return true
+		} else if len(fields) == 2 && strings.ToLower(fields[0]) == "/approve" && strings.ToLower(fields[1]) == "cancel" {
+			if approverSet.Has(*c.User.Login) {
+				approverSet.Delete(*c.User.Login)
 			}
 		}
-		p = path.Join(p, paths[i])
 	}
-	return false
+	return approverSet
+}
+
+// getApprovalNeededFiles identifies the files that still need approval from someone in their OWNERS files
+func (h ApprovalHandler) getApprovalNeededFiles(files []*goGithub.CommitFile, approverSet sets.String) sets.String {
+	needsApproval := sets.String{}
+	for _, file := range files {
+		if !h.isApproved(file, approverSet) {
+			needsApproval.Insert(*file.Filename)
+		}
+	}
+	return needsApproval
+}
+
+// isApproved indicates whether or not someone from the list of OWNERS for a file has approved the PR
+func (h ApprovalHandler) isApproved(someFile *goGithub.CommitFile, approverSet sets.String) bool {
+	fileOwners := h.features.Repos.Assignees(*someFile.Filename)
+	return fileOwners.Intersection(approverSet).Len() > 0
 }
