11bb6421926bb1afb6ce86aa5cf78e417e4cdb7c
Kenneth Owens <owensk@google.com>
2016-12-08 15:23:10 -0800

    Year updated in code header

--- statefulsets/zookeeper/zkGenConfig.sh
+++ statefulsets/zookeeper/zkGenConfig.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright YEAR The Kubernetes Authors.
+# Copyright 2016 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
